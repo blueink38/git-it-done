ec7a1c71947473e5340ca23878bcee8f8f8b7e8b
muncle <blueink38@yahoo.com>
2021-01-13 14:38:42 -0600

    updated from snapshot

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,16 +2,13 @@ var getUserRepos = function(user) {
   // format the github api url
   var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
-  // make a request to the url
+  // make a get request to url
   fetch(apiUrl).then(function(response) {
+    console.log(response);
     response.json().then(function(data) {
       console.log(data);
     });
   });
 };
 
-
- var response = fetch("https://api.github.com/users/octocat/repos");
-  console.log(response);
-  
-  getUserRepos();
+getUserRepos("lernantino");
